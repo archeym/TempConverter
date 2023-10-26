1cc73f381d5ecb2ac9f1333124af5ac0f8bda487
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-10-26 13:37:29 +0200

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by Arkadijs Makarenko on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
