98a3eac2b8d4a0826609a33ef075542f305bd3df
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-10-26 14:15:36 +0200

    Home Task init

---
+++ TempConverter/Controller/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by Arkadijs Makarenko on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var convertedTempLabel: UILabel!
+    @IBOutlet weak var tempSegementControl: UISegmentedControl!
+    
+    @IBOutlet weak var tempSlider: UISlider!{
+        didSet {
+            tempSlider.value = 0
+            tempSlider.minimumValue = 0
+            tempSlider.maximumValue = 100
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        convertedTempLabel.text = "32 ºF"
+    }
+    
+    @IBAction func slderValueChanged(_ sender: Any) {
+        
+        updateTempLabelFromSlider(value: tempSlider.value)
+    }
+    
+    func updateTempLabelFromSlider(value: Float) {
+        print("tempSlider:::", Int(value))
+        #warning("check which segment controler is on, and make a temp calculation based F,K")
+    }
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double){
+        
+        return (0, 0)
+    }
+    
+    #warning("pass result to InfoVC without segue")
+    
+
+}
+
